331094606e439399d9ebfbf377bb17b5a308aad1
roadsterdev <roadsterdev@outlook.com>
2018-06-12 09:13:50 -0400

    first commit

--- src/components/card/Card.js
+++ src/components/card/Card.js
@@ -4,6 +4,7 @@ import './Card.scss';
 class Card extends Component {
     render() {
         let {shape, color} = this.props;
+        
         return( 
             <div className="card">
             <div className={`${shape} ${color}`}></div>
