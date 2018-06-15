3afbcab6b4aa7eb115782b8dcb6c3a6e0c0c5a68
roadsterdev <roadsterdev@outlook.com>
2018-06-15 10:54:24 -0400

    more changes with frontend code

--- src/components/basicPack/BasicPack.js
+++ src/components/basicPack/BasicPack.js
@@ -3,24 +3,28 @@ import './BasicPack.scss';
 import BasicPackImg from './BasicPack.png';
 
 class BasicPack extends Component {
+
+    // handleClick() {
+    //     return (
+    //         <div className="overlay">
+    //             <div className="popup">
+    //                 <h2> Basic Pack </h2>
+    //                 <a className="close" href="#">x</a>
+    //                 <div className="content">
+    //                     This pack comes with 3 random basic cards.
+    //                 </div>
+    //             </div>
+    //         </div>
+    //     )
+    // }
     render() {
         return( 
             <div>
             <h4>Basic Pack</h4>
                 <div className="basic-pack">
-                   <a className="button" > 
+                   {/* <button onClick={this.handleClick}>  */}
                    <img src= {BasicPackImg} className="pack-of-cards" alt="Pack-of-Cards"/>
-                   </a>
-
-                   <div className="basicpackpopup overlay">
-                    <div className="popup">
-                        <h2> Basic Pack </h2>
-                        <a className="close" href="#"> &times;</a>x
-                        <div> className="content">
-                            This pack comes with 3 random basic cards.
-                        </div>
-                    </div>
-                    </div>
+                   {/* </button> */}
                    
                    
                 </div>
---
+++ src/components/pages/Dashboard.js
@@ -0,0 +1,106 @@
+import React, { Component } from 'react';
+import Card from './../card/Card';
+import Header from './../header/Header';
+import './Dashboard.scss';
+import Footer from './../footer/Footer';
+import TradingContainer from './../trade/tradingContainer';
+//import KaleidoKardsContract from './../../../build/contracts/KaleidoKards.json';
+import getWeb3 from './../../utils/getWeb3';
+
+
+
+
+class Dashboard extends Component {
+  constructor() {
+    super();
+
+    this.state = {
+      web3: null,
+      kaleidoKardsInstance: null,
+      cards: []
+
+    }
+  }
+
+//   componentWillMount () {
+
+//     console.log("here");
+//     getWeb3
+//     .then(results => {
+//       this.setState({
+//         web3: results.web3
+//       })
+
+//       // Instantiate contract once web3 provided.
+//       this.instantiateContract()
+//     })
+//     .catch((err) => {
+//       console.log(err)
+//     })
+//   }
+
+
+//   instantiateContract() {
+    
+//   //  const kaleidoKards = new this.state.web3.Contract(KaleidoKardsContract, 'contractAddress')
+//  //   console.log(kaleidoKards, "BANANA");
+  
+
+
+//     // Get accounts.
+//     this.state.web3.eth.getAccounts((error, accounts) => {
+// //     kaleidoKards.deployed().then((instance) => {
+//        instance.getKards().then((x) => console.log(x));
+//        this.setState({
+//          kaleidoKardsInstance: instance
+      
+//        })
+//       })
+//     })
+
+    
+//   }
+ 
+  // getCards() {
+  //   let result = getCards;
+  //   this.setState({cards: <Card shape= {result.shape} color={result.color} />})
+  // }
+
+  render() {
+    // let cards = this.state.cards;
+    return (
+      <div>
+        <Header/>
+        <div className= "App">
+          <div className = "square-container">
+            <div className="other-players-cards">
+              <h2> Trade Cards </h2>
+              <TradingContainer/> 
+            </div>
+            <div className="column-container">
+              <div className="ether">
+              </div>
+              <div className="my-cards">
+                <h2 className="my-cards-title"> My Cards </h2>
+                <div className="my-cards-container">
+                  <Card color="blue" shape="triangle"/>
+                  <Card color="light-blue" shape="circle"/>
+                  <Card color="orange" shape="star"/>
+                </div>
+              </div>
+              <div>
+                  {/* {cards}
+                  <button onClick={this.getCards}> Get Cards</button> */}
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <Footer/>
+
+      </div>
+    );
+  }
+}
+
+export default Dashboard;
