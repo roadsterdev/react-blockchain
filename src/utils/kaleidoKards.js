b55930d4ad604228aa384764c6c17f59cbe4fa04
roadsterdev <roadsterdev@outlook.com>
2018-06-19 17:02:57 -0400

    More better wrappers for web3/sol communication

--- src/utils/kaleidoKards.js
+++ src/utils/kaleidoKards.js
@@ -1,109 +1,161 @@
-import KaleidoKardsContract from '../build/contracts/KaleidoKards.json';
-import getWeb3 from './utils/getWeb3';
+import KaleidoKardsContract from '../../build/contracts/KaleidoKards.json';
+import getWeb3 from './getWeb3.js';
 
-let web3;
-let kaleidoKardsInstance;
+// let web3;
+// let kaleidoKardsInstance;
+//
+// let USER    = 'user_node';
+// let KAL     = 'kaleido_node'; //todo: consider renaming this globally to kal_node
+// let STORE   = 'kard_store_node';
 
-let USER    = 'user_node';
-let KAL     = 'kaleido_node'; //todo: consider renaming this globally to kal_node
-let STORE   = 'kard_store_node';
+// By default set the current node to the User
+// let current_node = USER;
 
 class KaleidoKards {
-
+// 0x702E797378A6A3c672374E58b2cf30a5220c2Fe7
     constructor() {
-        KaleidoKards.instantiateContract();
+        this.USER  = getWeb3('user_node');
+        this.UserContract = this.USER.then(response => {
+            return new response.eth.Contract(KaleidoKardsContract.abi, '0xb7a996f99afff30a8a7c5b95aa9617f0985da9ee');
+        });
+        this.KAL   = getWeb3('kaleido_node');
+        this.KalContract = this.KAL.then(response => {
+            return new response.eth.Contract(KaleidoKardsContract.abi, '0xb7a996f99afff30a8a7c5b95aa9617f0985da9ee');
+        });
+        this.STORE = getWeb3('kard_store_node');
+        this.StoreContract = this.STORE.then(response => {
+            return new response.eth.Contract(KaleidoKardsContract.abi, '0xb7a996f99afff30a8a7c5b95aa9617f0985da9ee');
+        });
     }
 
-    //TODO: need to switch user/node based on instance of web3
-    // consider passing param to getWeb3 to  switch users
-
-    // Functions for switching the node we're talking to
-    // returns this to allow chaining calls to this contract
-    // ex. asUser().getOwnedKards()
-    static asUser(){
-        this.updateNetwork(USER);
-        return this;
+    getAddress(node){
+        let config = Promise.all(this.getConfig(node));
+        return config.then( response => {
+            let web3 = response[0];
+            let contract = response[1];
+            return web3.eth.getAccounts();
+        }).then(accounts => {
+            return accounts[0];
+        });
     }
 
-    static asKardStore(){
-        this.updateNetwork(STORE);
-        return this;
-    }
-
-    static asKaleido(){
-        this.updateNetwork(KAL);
-        return this;
-    }
-
-    static updateNetwork(provider) {
-
-        getWeb3(provider)
-            .then(results => {
-                this.web3 = results.web3;
-                //TODO: get contract address dynamically
-                const kaleidoKards = new web3.eth.Contract(KaleidoKardsContract.abi, '0xb7a996f99afff30a8a7c5b95aa9617f0985da9ee');
-                this.kaleidoKardsInstance = kaleidoKards;
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-
-    }
-
-//returns a map of all the current user's kards
-// kardId => attribute
-// return usage ex. myKards[13].color, myKards[13].shape, etc
-    static getOwnedKards() {
-        let ret = new Promise(function (resolve, reject) {
-            var myKards = new Map();
-            this.kaleidoKardsInstance.methods.getOwnedKards("0x33F1ba1fa5F83Bc031E54bbBFBd2fF394707864C").call().then(
-                (response) => {
-                    //for each kard returned, get the values of the kard attributes
-                    response.forEach(function (kardIdString) {
-                        let kardId = parseInt(kardIdString);
-
-                        this.kaleidoKardsInstance.methods.getKard(kardId).call().then(
-                            (response) => {
-                                myKards[kardId] = response;
-                            })
-                            .catch(
-                                (error) => {
-                                    console.log(error);
-                                    reject(error);
-                                    //todo handle error
-                                }
-                            );
+    //node like 'user_node', to like address ex. 0xfqerg13498g283jf03kfh47
+    transfer(fromNode, toNode, kardId){
+        return this.getAddress(toNode).then((toAddress) => {
+            return this.getAddress(fromNode).then((fromAddress) => {
+                return this.getLastBlock(fromNode).then(lastBlock => {
+                    let config = Promise.all(this.getConfig(fromNode));
+                    return config.then( response => {
+                        let web3 = response[0];
+                        let contract = response[1];
+                        return contract.methods.transfer(toAddress, kardId).send({ from: fromAddress, gas: lastBlock.gasLimit });
                     });
                 })
-                .catch(
-                    (error) => {
-                        console.log("getOwnedKards failed");
-                        console.log(error);
-                        reject(error);
-                        //todo handle error
+            })
+        });
+    }
+
+    getLastBlock(node){
+        let config = Promise.all(this.getConfig(node));
+        return config.then( response => {
+            let web3 = response[0];
+            let contract = response[1];
+
+            return web3.eth.getBlock("latest");
+        });
+    }
+
+    //returns a map of all the current user's kards
+    // kardId => attribute
+    // return usage ex. myKards[13].color, myKards[13].shape, etc
+    getOwnedKards(node) {
+        let myKards = new Map();
+        let config = Promise.all(this.getConfig(node));
+        return config.then( response => {
+            let web3 = response[0];
+            let contract = response[1];
+
+            return web3.eth.getAccounts().then(accounts => {
+                return contract.methods.getOwnedKards(accounts[0]).call().then((response) => {
+                        return new Promise((resolve, reject) => {
+                            //for each kard returned, get the values of the kard attributes
+                            response.forEach(function (kardIdString) {
+                                let kardId = parseInt(kardIdString);
+                                contract.methods.getKard(kardId).call().then(
+                                    (kard) => {
+                                        // console.log(kard);
+                                        myKards[kardId] = kard;
+                                    });
+                            });
+                            resolve(myKards);
+                        })
                     });
-            resolve(myKards);
+            })
         })
     }
 
-    static buyStandardPack() {
-        let ret = new Promise(function (resolve, reject) {
 
-            this.kaleidoKardsInstance.methods.buyStandardPack().send({
-                from: "0x33F1ba1fa5F83Bc031E54bbBFBd2fF394707864C",
-                gas: 300000000,
-                value: web3.utils.toWei('1', 'ether')
-            }).then(function (receipt) {
-                console.log(receipt);
-                return true;
+
+    buyStandardPack(node) {
+        let config = Promise.all(this.getConfig(node));
+
+        return config.then( response => {
+            let web3 = response[0];
+            let contract = response[1];
+            return web3.eth.getBlock("latest").then( lastBlock => {
+                return web3.eth.getAccounts().then(accounts => {
+                    return contract.methods.buyStandardPack().send({
+                        from: accounts[0],
+                        gas: lastBlock.gasLimit,
+                        value: web3.utils.toWei('1', 'ether')
+                    })
+                })
             })
-                .catch((err) => {
-                    console.log(err);
-                    return false;
-                    //todo: handle error
-                });
-        })
+        }).then(function (txReceipt) {
+            // console.log(txReceipt); //TODO: use this to get tx data for frontend
+            return txReceipt;
+        }).catch((err) => {
+            console.log("buyStandardPack failed");
+            console.log(err);
+            return false;
+            //todo: handle error
+        });
+    }
+
+    buyPlatinumPack(node) {
+        let config = Promise.all(this.getConfig(node));
+
+        return config.then( response => {
+            let web3 = response[0];
+            let contract = response[1];
+            return web3.eth.getBlock("latest").then( lastBlock => {
+                return web3.eth.getAccounts().then(accounts => {
+                    return contract.methods.buyPlatinumPack().send({
+                        from: accounts[0],
+                        gas: lastBlock.gasLimit,
+                        value: web3.utils.toWei('1', 'ether') //todo: fix this cost somewhere
+                    })
+                })
+            })
+        }).then(function (txReceipt) {
+            console.log(txReceipt); //TODO: use this to get tx data for frontend
+            return true;
+        }).catch((err) => {
+            console.log("buyStandardPack failed");
+            console.log(err);
+            return false;
+            //todo: handle error
+        });
+    }
+
+    getConfig(node){
+        if (node === 'user_node') {
+            return [this.USER, this.UserContract];
+        } else if (node === 'kaleido_node'){
+            return [this.KAL, this.KalContract];
+        } else if (node === 'kard_store_node'){
+            return [this.STORE, this.StoreContract];
+        }
     }
 }
 
